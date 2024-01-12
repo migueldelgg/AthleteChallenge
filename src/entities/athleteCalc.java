3831ead79a39b48252df286f6e20471b1fc1f899
4Miguel2 <delgdevoleper@gmail.com>
2024-01-11 22:01:04 -0300

    athleteCalc ready ?

--- src/entities/athleteCalc.java
+++ src/entities/athleteCalc.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class athleteCalc extends athlete{
-    private List<athlete> athleteList = new ArrayList<>();
+    private static final List<athlete> athleteList = new ArrayList<>();
 
     public athleteCalc(String name, char gender, Double height, Double weight) {
         super(name, gender, height, weight);
@@ -19,16 +19,34 @@ public class athleteCalc extends athlete{
         }
         return (double) allWeight / qnt;
     }
-
-    public double higher(){
-        double higher = 0;
+    public double menPercentage(){
+        int qnt = 0;
+        int mans = 0;
 
         for(athlete c : athleteList) {
-            if (c.getHeight() > higher) {
-                higher = c.getHeight();
+            qnt++;
+            if (c.getGender() == 'M') {
+                mans += 1;
             }
         }
-        return higher;
+        return ((double) mans / qnt) * 100;
+    }
+
+    public static int qntWomen(){
+        int women = 0;
+        for(athlete c : athleteList) {
+            if (c.getGender() == 'F') {
+                women += 1;
+            }
+        }
+        return women;
+    }
+
+    public static String womenMsg() {
+        if (qntWomen() == 0) {
+            return "Não há mulheres na lista";
+        }
+        return String.valueOf(qntWomen());
     }
 
 
