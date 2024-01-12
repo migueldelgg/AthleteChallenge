55caea923325b838bb3585c066ddd50e5124e3f4
4Miguel2 <delgdevoleper@gmail.com>
2024-01-11 21:36:42 -0300

    entities almost ready

---
+++ src/entities/athlete.java
@@ -0,0 +1,50 @@
+package entities;
+
+public class athlete {
+    private String name;
+    private char gender;
+    private Double height;
+    private Double weight;
+
+    public athlete(){
+    }
+
+    public athlete(String name, char gender, Double height, Double weight) {
+        this.name = name;
+        this.gender = gender;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+}
---
+++ src/entities/athleteCalc.java
@@ -0,0 +1,24 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class athleteCalc extends athlete{
+    private List<athlete> athleteList = new ArrayList<>();
+
+    public athleteCalc(String name, char gender, Double height, Double weight) {
+        super(name, gender, height, weight);
+    }
+
+    public double middleWeight (){
+        int qnt = 0;
+        int allWeight = 0;
+        for(athlete c : athleteList) {
+            qnt++;
+            allWeight += getWeight();
+        }
+        return (double) allWeight / qnt;
+    }
+
+    
+}
